b9f2fbf16850427f1ee2c6945c1143ca2c8c8fc5
sl33pyturtl3 <zmcstro05@gmail.com>
2024-09-19 13:37:29 -0400

    mitty mitty twwooooo ttiiiimmmeessss

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,44 @@ public class Main {
     public static void main(String[] args)
 
     {
-        
+        int int0perandA = 32;
+        int int0perandB = 14;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intModulo = 0;
+
+        double double0perandA = 23.23;
+        double double0perandB = 3.14;
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+        double doubleModulo = 0;
+
+
+
+        doubleSum = int0perandA + int0perandB;
+        System.out.println("The sum using two ints of " + int0perandA + " and " + int0perandB + " is " + doubleSum);
+
+        intProduct = int0perandA * int0perandB;
+        System.out.println("The product using two ints of " + int0perandA + " and " + int0perandB + " is " + intProduct);
+
+        intDifference = int0perandA - int0perandB;
+        System.out.println("The difference using two ints of " + int0perandA + " and " + int0perandB + " is " + intDifference);
+
+        intQuotient = int0perandA / int0perandB;
+        System.out.println("The quotient using two ints of " + int0perandA + " and " + int0perandB + " is " + intQuotient);
+
+        intModulo = int0perandA % int0perandB;
+        System.out.println("The modulo using two ints of " + int0perandA + " and " + int0perandB + " is " + intModulo);
+
+
+    double lunchCost = 15.50;
+
+
+
     }
 }
