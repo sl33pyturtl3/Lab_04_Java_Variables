7caa593a6eb66df1e40035973ae1ac1b6aa49a9c
sl33pyturtl3 <zmcstro05@gmail.com>
2024-09-20 12:27:59 -0400

    mitty mitty three peat

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,25 @@ public class Main {
         int int0perandA = 32;
         int int0perandB = 14;
 
-        int intSum = 0;
-        int intProduct = 0;
-        int intDifference = 0;
-        int intQuotient = 0;
-        int intModulo = 0;
+        int intSum = 12;
+        int intProduct = 1;
+        int intDifference = 432;
+        int intQuotient = 32;
+        int intModulo = 43;
 
         double double0perandA = 23.23;
         double double0perandB = 3.14;
-        double doubleSum = 0;
-        double doubleProduct = 0;
-        double doubleDifference = 0;
-        double doubleQuotient = 0;
-        double doubleModulo = 0;
+
+        double doubleSum = 2.23;
+        double doubleProduct = 23.43;
+        double doubleDifference = 4.60;
+        double doubleQuotient = 6.00;
+        double doubleModulo = 94.90;
 
 
 
-        doubleSum = int0perandA + int0perandB;
-        System.out.println("The sum using two ints of " + int0perandA + " and " + int0perandB + " is " + doubleSum);
+        intSum = int0perandA + int0perandB;
+        System.out.println("The sum using two ints of " + int0perandA + " and " + int0perandB + " is " + intSum);
 
         intProduct = int0perandA * int0perandB;
         System.out.println("The product using two ints of " + int0perandA + " and " + int0perandB + " is " + intProduct);
@@ -37,9 +38,36 @@ public class Main {
         System.out.println("The modulo using two ints of " + int0perandA + " and " + int0perandB + " is " + intModulo);
 
 
+        doubleSum = double0perandA + double0perandB;
+        System.out.println("The sum using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleSum);
+
+        doubleProduct = double0perandA * double0perandB;
+        System.out.println("The product using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleProduct);
+
+        doubleDifference = double0perandA - double0perandB;
+        System.out.println("The difference using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleDifference);
+
+        doubleQuotient = double0perandA / double0perandB;
+        System.out.println("The quotient using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleQuotient);
+
+        doubleModulo = double0perandA % double0perandB;
+        System.out.println("The modulo using two doubles of " + double0perandA + " and " + double0perandB + " is " + doubleModulo);
+
+
     double lunchCost = 15.50;
-
-
-
+        System.out.println("The cost of lunch is " + lunchCost);
+    int familyCount = 5;
+        System.out.println("There are " + familyCount + " people in my family.");
+    boolean isRaining = false;
+    double gasPrice = 3.34;
+        System.out.println("A gallon of gas costs $" + gasPrice);
+    int favNumber = 21;
+        System.out.println("My favorite number is " + favNumber);
+    int shoeSize = 10;
+        System.out.println("My shoe size is " + shoeSize);
+    String birthMonth = "December";
+        System.out.println("I was born in " + birthMonth);
+    String fullName = "Zach Castro";
+        System.out.println("My name is " + fullName);
     }
 }
